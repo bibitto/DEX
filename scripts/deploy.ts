5840fe7e51e3fb9e39acae9a2c51adc4b91b2e98
bibitto <bibitto.crypto@gmail.com>
2022-09-15 23:17:17 +0900

    fix deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,9 +8,21 @@ async function main() {
 
   // ERC20(liquidity token)
   const BibittoSwapERC20 = await ethers.getContractFactory('BibittoSwapERC20');
-  const erc20 = await BibittoSwapERC20.deploy(unlockTime);
+  const erc20 = await BibittoSwapERC20.deploy();
   await erc20.deployed();
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${erc20.address}`);
+  console.log(`deployed to ${erc20.address}`);
+
+  // Factory
+  const BibittoSwapFactory = await ethers.getContractFactory('BibittoSwapFactory');
+  const factory = await BibittoSwapFactory.deploy('0x5FbDB2315678afecb367f032d93F642f64180aa3');
+  await factory.deployed();
+  console.log(`deployed to ${factory.address}`);
+
+  // Pair
+  const BibittoSwapPair = await ethers.getContractFactory('BibittoSwapPair');
+  const pair = await BibittoSwapPair.deploy();
+  await pair.deployed();
+  console.log(`deployed to ${pair.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
